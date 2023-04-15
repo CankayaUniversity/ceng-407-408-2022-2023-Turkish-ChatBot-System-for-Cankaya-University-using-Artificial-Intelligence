69a5c280ec74d4c86851360196bfa5a6481afa46
NightSwindlerz <sokemete@gmail.com>
2023-04-15 22:38:16 +0300

    basic chatbot interface created and added to main

--- ai_chatbot_website/src/main.jsx
+++ ai_chatbot_website/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import FullChatbot from './FullChatbot'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FullChatbot />
   </React.StrictMode>,
 )
