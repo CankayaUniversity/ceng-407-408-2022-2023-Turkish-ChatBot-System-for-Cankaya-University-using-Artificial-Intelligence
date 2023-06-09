56ed5beb3b18a738c7aaf60b28c34b8a6775df68
Meryem Ozge Uzun <76754183+mozgeuzun@users.noreply.github.com>
2023-06-09 23:51:47 +0300

    Elasticsearch added

--- backend.py
+++ backend.py
@@ -5,6 +5,9 @@ import openai
 import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 import traceback 
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
 
 app = Flask(__name__)
 CORS(app)
@@ -17,8 +20,51 @@ tokenizer = AutoTokenizer.from_pretrained("savasy/bert-base-turkish-squad")
 model = AutoModelForQuestionAnswering.from_pretrained("savasy/bert-base-turkish-squad")
 nlp = pipeline("question-answering", model=model, tokenizer=tokenizer)
 
-with open("passage.txt", "r", encoding="utf-8") as f:
-    text = f.read()
+# Connect to Elasticsearch
+es = Elasticsearch(
+    hosts=['https://localhost:9200'],
+    http_auth=('elastic', 'fIs*jD*3To+IpzLP3D+B'),
+    verify_certs=False
+)
+
+# Create index mapping
+index_name = 'passages'
+index_mapping = {
+    "mappings": {
+        "properties": {
+            "passage_id": {"type": "integer"},
+            "passage": {"type": "text", "analyzer": "turkish"},
+        }
+    }
+}
+
+# Create the index with the custom mapping if it doesn't exist
+if not es.indices.exists(index=index_name):
+    es.indices.create(index=index_name, body=index_mapping)
+
+
+# Index the passages
+def index_passages():
+    with open("passage.txt", "r", encoding="utf-8") as f:
+        passages = f.readlines()
+
+    bulk_data = []
+    for i, passage in enumerate(passages):
+        doc = {
+            'passage_id': i,
+            'passage': passage.strip()
+        }
+        bulk_data.append({
+            "_index": index_name,
+            "_id": i,
+            "_source": doc
+        })
+
+    bulk(es, bulk_data)
+
+# Call the function to index the passages
+index_passages()
+
 
 @app.route("/api/status", methods=["GET"])
 def get_status():
@@ -52,12 +98,34 @@ def get_gpt3_response():
 
 @app.route("/generate-response-bert", methods=["POST"])
 def get_bert_response():
-    message = request.json["message"]
+    question = request.json["message"]
+    # Search for relevant passages using Elasticsearch
+    search_results = es.search(index=index_name, body={
+        'query': {
+            'match': {
+                'passage': {
+                    'query': question,
+                    'analyzer': 'turkish'
+                }
+            }
+        },
+        'size': 5  # Return top 5 passages
+    })
+
     try:
-        print("Message:", message)
-        print("Context:", text)
-        response = nlp(question=message, context=text)
-        return jsonify({"text": response['answer']})
+        passages = '\n'.join([hit['_source']['passage'] for hit in search_results['hits']['hits']])
+    
+        if not passages:
+            return jsonify({'text': 'Üzgünüm ne dediğinizi anlayamadım. Tekrar sorunuzu sorabilir misiniz?'}), 200
+
+        results = nlp(question=question, context=passages, top_k=5)
+        result = max(results, key=lambda x: len(x['answer']))
+
+        if result is None:
+            return jsonify({'text': 'Üzgünüm ne dediğinizi anlayamadım. Tekrar sorunuzu sorabilir misiniz?'}), 200
+        else:
+            return jsonify({'text': result['answer']}), 200
+
     except Exception as e:
         print(traceback.format_exc())
         return jsonify({"error": "Error generating response"}), 500
