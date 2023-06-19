e3366eeb1642ce40ab3ba167d5cb63500d736be1
NightSwindlerz <sokemete@gmail.com>
2023-06-19 22:55:53 +0300

    Chatbot design improved

--- ChatbotComparison/src/ChatbotContainer.jsx
+++ ChatbotComparison/src/ChatbotContainer.jsx
@@ -4,14 +4,14 @@ import './ChatbotButton.css'; // New CSS for the buttons
 
 const ChatbotContainer = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [model, setModel] = useState("BERT");
+  const [model, setModel] = useState("GPT-3");
 
   const handleClick = () => setIsVisible(!isVisible);
-  const handleModelChange = () => setModel(model === "BERT" ? "GPT-3" : "BERT");
+  const handleModelChange = () => setModel(model === "GPT-3" ? "GPT-3" : "BERT");
 
-  const apiUrl = model === "BERT"
-    ? "http://localhost:5000/generate-response-bert"
-    : "http://localhost:5000/generate-response-gpt3";
+  const apiUrl = model === "GPT-3"
+    ? "http://localhost:5000/generate-response-gpt3"
+    : "http://localhost:5000/generate-response-bert";
 
   return (
     <div style={{ position: 'fixed', right: '20px', bottom: '20px' }}>
@@ -19,7 +19,7 @@ const ChatbotContainer = () => {
         ? (
           <>
             <FullChatbot title={`Model: ${model}`} apiUrl={apiUrl} closeChatbot={handleClick} />
-            <button onClick={handleModelChange}>Switch to {model === "BERT" ? "GPT-3" : "BERT"}</button>
+            {/* <button onClick={handleModelChange}>Switch to {model === "BERT" ? "GPT-3" : "BERT"}</button> */}
           </>
         )
         : <img src='src/assets/bee.jpg'  width="90" alt='Open chatbot' className='chatbot-button' onClick={handleClick} />
